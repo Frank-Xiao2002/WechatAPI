0b01b3b07295bc8fe6cfe70510d486a19914d09d
Frank-Xiao2002 <Xiao670127@126.com>
2024-04-21 23:36:34 +0800

    use text block for static message

--- src/main/java/dev/xxj/wechatapi/service/MessageService.java
+++ src/main/java/dev/xxj/wechatapi/service/MessageService.java
@@ -28,9 +28,10 @@ public class MessageService {
         Document doc = factory.newDocumentBuilder().parse(inputSource);
         String ToUserName = doc.getElementsByTagName("ToUserName").item(0).getTextContent();
         String FromUserName = doc.getElementsByTagName("FromUserName").item(0).getTextContent();
-        String content = "您好，欢迎关注xxj的公众号！\n" +
-                "求职相关信息请访问网页：http://120.46.182.90/wechatapi/web/\n" +
-                "感谢您对本公众号的支持！";
+        String content = """
+                您好，欢迎关注xxj的公众号！
+                求职相关信息请访问网页：http://120.46.182.90/wechatapi/web/
+                感谢您对本公众号的支持！""";
         String result = responseBody.replace("TOUSERNAME", FromUserName)
                 .replace("FROMUSERNAME", ToUserName)
                 .replace("CREATETIME", String.valueOf(System.currentTimeMillis()))
